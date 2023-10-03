03a69f073f83ebeba4ba23ef09ff9f4d8c8a9770
Ilie-Iulian-Mihai <iulian.m.ilie@gmail.com>
2023-10-03 18:25:19 +0300

    working on setting up the api - adding /decks endpoint plus dotenv to secure the app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,43 @@
-import express from "express";
+import { config } from "dotenv";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Deck from "./models/Deck";
 
-const app = express();
+config();
 
 const PORT = 5000;
+const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+// allowing support for json post request (! PUT BEFORE ENDPOINTS ↓)
+app.use(express.json());
+
+// API's settup
+app.post("/decks", async (req: Request, res: Response) => {
+  console.log(req.body);
+
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
 });
 
-app.listen(PORT);
+app.get("/decks", async (req, res) => {
+  try {
+    const decks = await Deck.find();
+    res.json(decks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Muie la cai");
+});
+
+// Database connection
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`Connected on port ${PORT}`);
+  app.listen(PORT);
+});
